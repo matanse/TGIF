1dd3cc325d9d0d69ed118d11c45fd07f09e5a912
mat <matan.selig.ubiqum@gmail.com>
2020-01-29 16:55:52 +0100

    strting to take information from from j to html

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,15 @@
-document.getElementById("senate-data").innerHTML = JSON.stringify(
-  data,
-  null,
-  2
-);
+const members = data.results[0].members;
+
+const print_members = arr => {
+  for (i in arr) {
+    if (members[i].middle_name == null) {
+      let full_name = `${members[i].first_name} ${members[i].last_name}`;
+      console.log(full_name);
+    } else {
+      let full_name = `${members[i].first_name} ${members[i].middle_name} ${members[i].last_name}`;
+      console.log(full_name);
+    }
+  }
+};
+
+print_members(members);
